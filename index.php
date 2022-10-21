76badaa2e0b98fc19bf48657d1441fa953172b39
Aarthy Varathan <110882378+Aarthy-V@users.noreply.github.com>
2022-10-21 21:55:07 +0530

    Add files via upload
    Project

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Medical Center UOJ</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<style>
+	h1 {text-align: left;}
+	img {
+  		width: 100%;
+	}
+</style>
+<style>
+body {
+  background-image: url('home.webp');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+ 
+}
+</style>
+
+</head>
+
+
+<body>
+	<ul>
+		<li><a href="login.php" class="btn btn-success" >LOGIN</a></li>
+		
+	</ul>
+	<br>
+	<br>
+	 
+	<h1 style="color:deepskyblue" style="font-family:Trebuchet MS"><i>&nbsp &nbsp --- WELCOME TO THE MEDICAL CENTER ---</h1>
+		<br><br>
+	<h1 style="color:deepskyblue;"><i>&nbsp &nbsp &nbsp &nbsp UNIVERSITY OF JAFFNA</h1>
+
+	</h1>
+	
+	
+	
+
+
+</body>
+</html>
